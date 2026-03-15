593461a8037ced96ff19043001573625ef574dc1
sreeram <sreenu.ram2@gmail.com>
2026-03-15 11:54:04 +0530

    list operations and commnets

---
+++ 9_listOperations.py
@@ -0,0 +1,18 @@
+my_list = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
+#  accessing element
+print(my_list[0])
+print(my_list[1])
+print(my_list[-1])
+
+# add new element
+my_list.append("Jul")
+print(my_list)
+
+#  commenta # or with in for multi line """ """
+"""  remove last element
+my_list.remove("Jul") 
+my_list.pop()
+print(my_list)"""
+
+
+
