1a63b8c495059f96b166c944b741ce4fc4d07e9f
sreeram <sreenu.ram2@gmail.com>
2026-03-19 12:06:02 +0530

    copy deep copy list

--- python-tutorialpoint/6_lamdaList.py
+++ python-tutorialpoint/6_lamdaList.py
@@ -30,3 +30,35 @@ print (chars)
 # Clarity − List comprehensions result in clearer and more expressive code, making it easier to understand the purpose and logic of the operation being performed.
 #
 # Reduced Chance of Errors − Since list comprehensions are more compact, there is less chance of errors compared to traditional for loops, reducing the likelihood of bugs in your code.
+
+import copy
+# Original list
+original_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# Creating a shallow copy
+shallow_copied_list = copy.copy(original_list)
+# Modifying an element in the shallow copied list
+shallow_copied_list[0][0] = 100
+# Printing both lists
+print("Original List:", original_list)
+print("Shallow Copied List:", shallow_copied_list)
+
+import copy
+# Original list
+original_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# Creating a deep copy
+deep_copied_list = copy.deepcopy(original_list)
+# Modifying an element in the deep copied list
+deep_copied_list[0][0] = 100
+# Printing both lists
+print("Original List:", original_list)
+print("Deep Copied List:", deep_copied_list)
+
+# Original list
+original_list = [1, 2, 3, 4, 5]
+# Copying the list using slice notation
+copied_list = original_list[1:4]
+# Modifying the copied list
+copied_list[0] = 100
+# Printing both lists
+print("Original List:", original_list)
+print("Copied List:", copied_list)
