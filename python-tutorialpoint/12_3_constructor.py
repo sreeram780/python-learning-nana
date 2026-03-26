25e1289c03e6919efd092a255884bf610ed57a04
sreeram <sreenu.ram2@gmail.com>
2026-03-26 19:49:15 +0530

    access modifiers and getter setter methods

--- python-tutorialpoint/12_3_constructor.py
+++ python-tutorialpoint/12_3_constructor.py
@@ -75,3 +75,45 @@ st2 = Student("Ram", 25)
 print(f"Name: {st2.name} and Age: {st2.age}")
 st3 = Student("Shyam", 26, "M")
 print(f"Name: {st3.name}, Age: {st3.age} and Gender: {st3.gender}")
+
+# Access Modifiers in Python
+class Employee:
+   def __init__(self, name, age, salary):
+      self.name = name # public variable
+      self.__age = age # private variable
+      self._salary = salary # protected variable
+   def displayEmployee(self):
+      print ("Name : ", self.name, ", age: ", self.__age, ", salary: ", self._salary)
+e1=Employee("Bhavana", 24, 10000)
+print (e1.name)
+print (e1._salary)
+
+# Name Mangling
+# obj._class__privatevar
+print (e1._Employee__age)
+
+# Getters and Setter Methods
+class Employee:
+   def __init__(self, name, age):
+      self.__name = name
+      self.__age = age
+
+   def get_name(self):
+      return self.__name
+   def get_age(self):
+      return self.__age
+   def set_name(self, name):
+      self.__name = name
+      return
+   def set_age(self, age):
+      self.__age=age
+      return
+   name = property(get_name, set_name, "name")
+   age = property(get_age, set_age, "age")
+
+e1=Employee("Bhavana", 24)
+print ("Name:", e1.name, "age:", e1.age)
+
+e1.name = "Archana"
+e1.age = 23
+print ("Name:", e1.name, "age:", e1.age)
