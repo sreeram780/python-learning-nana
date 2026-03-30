f4a410a0c19715717ecbc0b82ece001ce0a44cfb
sreeram <sreenu.ram2@gmail.com>
2026-03-30 17:25:58 +0530

    Implement Method Overloading Using MultipleDispatch

--- python-tutorialpoint/12_7_Polymorphism.py
+++ python-tutorialpoint/12_7_Polymorphism.py
@@ -63,3 +63,19 @@ result2 = add(10, 25, 35)
 
 print(result1)
 print(result2)
+
+# Implement Method Overloading Using MultipleDispatch
+from multipledispatch import dispatch
+class example:
+   @dispatch(int, int)
+   def add(self, a, b):
+      x = a+b
+      return x
+   @dispatch(int, int, int)
+   def add(self, a, b, c):
+      x = a+b+c
+      return x
+obj = example()
+
+print (obj.add(10,20,30))
+print (obj.add(10,20))
