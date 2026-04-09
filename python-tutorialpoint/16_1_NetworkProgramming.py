448a50dcdfd2ad471aa626e134c43412293fe218
sreeram <sreenu.ram2@gmail.com>
2026-04-09 15:19:27 +0530

    network programming

---
+++ python-tutorialpoint/16_1_NetworkProgramming.py
@@ -0,0 +1,11 @@
+# Python also has libraries that provide higher-level access to specific application-level network protocols, such as FTP, HTTP, and so on.
+#
+# Protocol	Common function	Port No	Python module
+# HTTP	Web pages	80	httplib, urllib, xmlrpclib
+# NNTP	Usenet news	119	nntplib
+# FTP	File transfers	20	ftplib, urllib
+# SMTP	Sending email	25	smtplib
+# POP3	Fetching email	110	poplib
+# IMAP4	Fetching email	143	imaplib
+# Telnet	Command lines	23	telnetlib
+# Gopher	Document transfers	70	gopherlib, urllib
