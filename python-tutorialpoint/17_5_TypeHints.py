09e2ae70cf5f2edaed6e08a4efdc9c350135c69f
sreeram <sreenu.ram2@gmail.com>
2026-04-12 09:22:58 +0530

    callable types

--- python-tutorialpoint/17_5_TypeHints.py
+++ python-tutorialpoint/17_5_TypeHints.py
@@ -88,3 +88,19 @@ print_value("hello")
 print_value(True)
 print_value([1, 2, 3])
 print_value({'key': 'value'})
+
+# Callable types
+from typing import Callable
+# Define a function that takes another function as an argument
+def apply_operation(x: int, y: int, operation: Callable[[int, int], int]) -> int:
+   return operation(x, y)
+# Example functions to be passed as arguments
+def add(a: int, b: int) -> int:
+   return a + b
+def multiply(a: int, b: int) -> int:
+   return a * b
+# Using the apply_operation function with different operations
+result1 = apply_operation(5, 3, add)        # result1 will be 8
+result2 = apply_operation(5, 3, multiply)   # result2 will be 15
+print(result1)
+print(result2)
