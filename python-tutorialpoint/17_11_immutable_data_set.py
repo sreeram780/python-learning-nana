e82200b63aa468814a11b64658a23efe83505d46
sreeram <sreenu.ram2@gmail.com>
2026-04-20 22:13:15 +0530

    Understanding Frozen Sets Immutability in Python

--- python-tutorialpoint/17_11_immutable_data_set.py
+++ python-tutorialpoint/17_11_immutable_data_set.py
@@ -43,3 +43,19 @@ try:
    my_string[0] = 'h'  # TypeError: 'str' object does not support item assignment
 except TypeError as e:
    print(f"Error: {e}")
+
+
+# Creating a frozenset
+frozen_set = frozenset([1, 2, 3, 4])
+# Attempting to add an element to the frozenset will raise an error
+try:
+   frozen_set.add(5)
+except AttributeError as e:
+   print(f"Error: {e}")
+# Attempting to remove an element from the frozenset will also raise an error
+try:
+   frozen_set.remove(2)
+except AttributeError as e:
+   print(f"Error: {e}")
+# The original frozenset remains unchanged
+print("Original frozenset:", frozen_set)
