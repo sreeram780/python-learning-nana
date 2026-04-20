e1255a73719c8669b733a67a7658748796cf7180
sreeram <sreenu.ram2@gmail.com>
2026-04-20 22:01:29 +0530

    immutable dataset

---
+++ python-tutorialpoint/17_11_immutable_data_set.py
@@ -0,0 +1,7 @@
+empty_tuple = ()
+single_element_tuple = (5,)  # Note the comma after the single element
+print("Single element tuple:", single_element_tuple)
+multi_element_tuple = (1, 2, 'Tutorialspoint', 3.14)
+print("Multi elements tuple:", multi_element_tuple)
+nested_tuple = (1, (2, 3), 'Learning')
+print("Nested tuple:", nested_tuple)
