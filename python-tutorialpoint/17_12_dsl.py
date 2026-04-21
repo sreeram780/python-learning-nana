e2a48129546061c7094f838d59907177ab026911
sreeram <sreenu.ram2@gmail.com>
2026-04-21 11:31:05 +0530

    example-2 foe DSL in python

--- python-tutorialpoint/17_12_dsl.py
+++ python-tutorialpoint/17_12_dsl.py
@@ -13,3 +13,16 @@ def add(x, y):
 def multiply(x, y):
     return x * y
 print(add(multiply(1, 3), multiply(2, 4)))
+
+# Example 2
+class demo:
+    def __init__(self):
+        self.settings = {}
+    def set(self, key, value):
+        self.settings[key] = value
+        return self
+    def get(self, key):
+        return self.settings.get(key)
+result = demo()
+result.set("host", "Welcome").set("port", 1231)
+print(result.get("host"))
